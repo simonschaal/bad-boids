625520368c84ad312e9e6f95b6131123500c1a95
Simon Schaal <nomis05@gmail.com>
2015-12-22 20:37:06 +0000

    Add first simple unit tests

--- boids/boids.py
+++ boids/boids.py
@@ -1,7 +1,6 @@
 
 from matplotlib import pyplot as plt
 from matplotlib import animation
-import random
 import numpy as np
 
 
@@ -13,37 +12,38 @@ class Boids(object):
         self._vxy_low_lim=np.array([vxlimits[0], vylimits[0]])
         self._vxy_up_lim=np.array([vxlimits[1], vylimits[1]])
         # generate random boid positions and velocities within given limits
-        self.pos = self.gen_random(self.boid_count, self._xy_low_lim, self._xy_up_lim)
-        self.vel = self.gen_random(self.boid_count, self._vxy_low_lim, self._vxy_up_lim)
+        self.positions = self.gen_random(self.boid_count, self._xy_low_lim, self._xy_up_lim)
+        self.velocities = self.gen_random(self.boid_count, self._vxy_low_lim, self._vxy_up_lim)
 
     def gen_random(self, count, lower_limits, upper_limits):
         width=upper_limits-lower_limits
         return (lower_limits[:,np.newaxis] + 
             np.random.rand(2, count)*width[:,np.newaxis])
 
-    def _fly_towards_middle(self, positions, strength):
+    def _fly_towards_middle(self, strength):
         # Fly towards the middle
-        middle=np.mean(positions, 1)
-        direction_to_middle = positions - middle[:, np.newaxis]
+        middle=np.mean(self.positions, 1)
+        direction_to_middle = self.positions - middle[:, np.newaxis]
         return (-1) * direction_to_middle * strength
 
-    def _avoid_collisions(self, positions, alert_distance):
+    def _avoid_collisions(self, alert_distance):
         # Fly away from nearby boids
-        separations = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
+        separations = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
         squared_displacements = separations * separations
         square_distances = np.sum(squared_displacements, 0)
+        print square_distances
         far_away = square_distances > alert_distance
         separations_if_close = np.copy(separations)
         separations_if_close[0,:,:][far_away] =0
         separations_if_close[1,:,:][far_away] =0
         return np.sum(separations_if_close,1) 
 
-    def _match_speed_nearby(self, positions, velocities, formation_flying_distance, formation_flying_strength):
+    def _match_speed_nearby(self, formation_flying_distance, formation_flying_strength):
    	    # Try to match speed with nearby boids
-        separations = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
+        separations = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
         squared_displacements = separations * separations
         square_distances = np.sum(squared_displacements, 0)
-        velocity_differences = velocities[:,np.newaxis,:] - velocities[:,:,np.newaxis]
+        velocity_differences = self.velocities[:,np.newaxis,:] - self.velocities[:,:,np.newaxis]
         very_far=square_distances > formation_flying_distance
         velocity_differences_if_close = np.copy(velocity_differences)
         velocity_differences_if_close[0,:,:][very_far] =0
@@ -51,26 +51,26 @@ class Boids(object):
         return (-1) * np.mean(velocity_differences_if_close, 1) * formation_flying_strength
        
 
-    def update_boids(self, positions, velocities):
-        velocities += self._fly_towards_middle(positions, 0.01)  
-        velocities += self._avoid_collisions(positions, 100) 
-        velocities += self._match_speed_nearby(positions, velocities, 10000, 0.125)
+    def update_boids(self):
+        self.velocities += self._fly_towards_middle(0.01)  
+        self.velocities += self._avoid_collisions(100) 
+        self.velocities += self._match_speed_nearby(10000, 0.125)
         #update positions acc to models
-        positions += velocities
+        self.positions += self.velocities
 
-    def animate(self,frame, scatter):
-        self.update_boids(self.pos, self.vel)
-        scatter.set_offsets(zip(self.pos[0], self.pos[1]))
+    def _animate(self,frame, scatter):
+        self.update_boids()
+        scatter.set_offsets(zip(self.positions[0], self.positions[1]))
 
+    def simulate(self, frames=50, interval=50, xlimits=(-500, 1500), ylimits=(-500, 1500)):
+        figure=plt.figure()
+        axes=plt.axes(xlim=xlimits, ylim=ylimits)
+        scatter=axes.scatter(self.positions[0],self.positions[1])
+        anim = animation.FuncAnimation(figure, self._animate, fargs=[scatter], 
+                                       frames=frames, interval=interval)
+        plt.show()
 
-boids=Boids(50)
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids.pos[0],boids.pos[1])
-
-
-anim = animation.FuncAnimation(figure, boids.animate, fargs=[scatter], 
-                               frames=50, interval=50)
 
 if __name__ == "__main__":
-    plt.show()
+    boids=Boids(50)
+    boids.simulate()
--- boids/test/test_boids.py
+++ boids/test/test_boids.py
@@ -3,14 +3,17 @@ from nose.tools import assert_almost_equal, assert_equal
 import os
 import yaml
 import numpy as np
+import random
 
 def test_bad_boids_regression():
-    boids=Boids(0)
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixtures','fixture.yml')))
     boid_data=regression_data["before"]
     positions=np.array([boid_data[0], boid_data[1]])
     velocities=np.array([boid_data[2], boid_data[3]])
-    boids.update_boids(positions, velocities)
+    boids=Boids(0)
+    boids.positions=positions
+    boids.velocities=velocities
+    boids.update_boids()
     for after,before in zip(regression_data["after"],positions.tolist()+velocities.tolist()):
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.02)
@@ -27,4 +30,39 @@ def test_gen_random():
     assert_equal(len(y), count)
     assert max(y) <= up[1]
     assert min(y) >= low[1]
+
+def test_fly_towards_middle():
+    # generate random range data
+    length=random.randint(10,50)
+    boids=Boids(length)
+    boids.positions=np.array([random.random()*np.arange(length), random.random()*np.arange(length)])
+    fly_middle=boids._fly_towards_middle(random.random())
+
+    # check length
+    assert_equal(fly_middle.size, 2*length)
+    # for range variables it should sum to zero due to symmetry
+    assert_almost_equal(np.sum(fly_middle), 0, delta = 1e-10)
+
+def test_avoid_collisions():
+    # generate random range data
+    length=random.randint(10,50)
+    boids=Boids(length)
+    boids.positions=np.array([100*random.random()*np.arange(length), 100*random.random()*np.arange(length)])
+    collisions=boids._avoid_collisions(1000)
+
+    # check length
+    assert_equal(collisions.size, 2*length)
+    # for range variables it should sum to zero due to symmetry
+    assert_almost_equal(np.sum(collisions), 0, delta = 1e-10)
+
+def test_match_Speed():
+    # generate random range data
+    length=random.randint(10,50)
+    boids=Boids(length)
+    speed=boids._match_speed_nearby(100,100)
+
+    # check length
+    assert_equal(speed.size, 2*length)
+    # for range variables it should sum to zero due to symmetry
+    assert_almost_equal(np.sum(speed), 0, delta = 1e-10)
 
